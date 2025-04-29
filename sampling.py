cfe87cd9dea08dd3063c5b872612d9e4a68b84ce
Etyl <etyl.sani@gmail.com>
2025-04-29 17:09:19 +0100

    fixed sampling

--- sampling.py
+++ sampling.py
@@ -5,7 +5,7 @@ from mesh import Mesh, MeshPoint
 
 def area_triangle(p0,p1,p2):
     v1 = p1 - p0
-    v2 = p2 - p1
+    v2 = p2 - p0
     
     # Calculate the cross product of the two vectors
     cross_product = np.cross(v1, v2)
@@ -31,7 +31,7 @@ def uniform_sampling(mesh: Mesh, n_points: int):
         The sampled points.
     """
     weights = np.zeros(len(mesh.triangles))
-    for triangle in mesh.triangles:
+    for triangle in range(len(mesh.triangles)):
         weights[triangle] += area_triangle(
             mesh.positions[mesh.triangles[triangle][0]],
             mesh.positions[mesh.triangles[triangle][1]],
@@ -40,10 +40,12 @@ def uniform_sampling(mesh: Mesh, n_points: int):
     
     samples = []
     weights = weights / weights.sum()
+
     for k in range(n_points):
         triangle_id = np.random.choice(len(mesh.triangles), p=weights)
-        barycentric_coords = np.random.rand(3)
-        barycentric_coords = barycentric_coords / np.sum(barycentric_coords)
-        samples.append(MeshPoint(triangle_id, barycentric_coords[:2]))
+        barycentric_coords = np.zeros(2)
+        barycentric_coords[0] = np.random.random()
+        barycentric_coords[1] = (1-barycentric_coords[0])*np.random.random()
+        samples.append(MeshPoint(triangle_id, barycentric_coords))
 
     return samples
