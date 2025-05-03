f5adcd31d23ce1aaa5394c3f440d37d83bfa0be3
Etyl <etyl.sani@gmail.com>
2025-05-03 19:44:17 +0100

    cleaned main file

--- src/main.py
+++ src/main.py
@@ -4,19 +4,15 @@ import os
 from tqdm import tqdm
 
 from geometry.trace_geodesic import straightest_geodesic
-from geometry.mesh import MeshPoint, Mesh
-from dataloader.mesh_loader import create_tetrahedron, load_mesh_from_obj
+from dataloader.mesh_loader import load_mesh_from_obj
 from ui import visualize_mesh_and_path
 from geometry.sampling import uniform_sampling
 from constants import DATA_DIR
 
 os.environ['PYTHONUNBUFFERED'] = '1'
 
-def main():
-    # mesh = create_tetrahedron()
+def main(n_points=10000, visualize=False):
     mesh = load_mesh_from_obj(os.path.join(DATA_DIR, "cat_head.obj"))
-
-    n_points = 10000
 
     samples = uniform_sampling(mesh, n_points)
     directions = [
@@ -31,13 +27,15 @@ def main():
         start = samples[idx]
         dir = directions[idx]
         path = straightest_geodesic(mesh, start, dir)
-        # paths.append(path)
+        if visualize:
+            paths.append(path)
 
     t = time.perf_counter() - t0
     print(f"Total time elapsed: {t}")
     print(f"Average per trace: {t/n_points}")
 
-    # visualize_mesh_and_path(mesh, paths)
+    if visualize:
+        visualize_mesh_and_path(mesh, paths)
 
 if __name__ == "__main__":
     main()
