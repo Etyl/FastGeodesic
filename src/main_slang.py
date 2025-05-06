4d00a5476e7fd918dc76b093408a8bd97f818b14
Etyl <etyl.sani@gmail.com>
2025-05-06 22:31:20 +0100

    feat: slang mesh loading

--- src/main_slang.py
+++ src/main_slang.py
@@ -4,15 +4,26 @@ import os
 
 from dataloader.mesh_loader import load_mesh_from_obj
 from constants import DATA_DIR
-from geometry.mesh import Mesh
+
 
 def load_mesh(module, device):
-    mesh:Mesh = load_mesh_from_obj(os.path.join(DATA_DIR,"cat_head.obj"))
+    mesh = load_mesh_from_obj(os.path.join(DATA_DIR,"cat_head.obj"))
+    
     positions = spy.NDBuffer(device, dtype=np.double, shape=mesh.positions.shape)
     triangles = spy.NDBuffer(device, dtype=np.int32, shape=mesh.triangles.shape)
     adjacencies = spy.NDBuffer(device, dtype=np.int32, shape=mesh.adjacencies.shape)
     triangle_normals = spy.NDBuffer(device, dtype=np.double, shape=mesh.triangle_normals.shape)
-    v2t = spy.NDBuffer(device, dtype=np.double, shape=mesh.triangle_normals.shape)
+    v2t = spy.NDBuffer(device, dtype=np.double, shape=mesh.v2t.shape)
+
+    positions.copy_from_numpy(mesh.positions)
+    triangles.copy_from_numpy(mesh.triangles)
+    adjacencies.copy_from_numpy(mesh.adjacencies)
+    triangle_normals.copy_from_numpy(triangle_normals)
+    v2t.copy_from_numpy(mesh.v2t)
+
+    slang_mesh = module.Mesh(positions, triangles, adjacencies, triangle_normals, v2t)
+    return slang_mesh
+
 
 
 def main():
@@ -24,5 +35,8 @@ def main():
     # Load the module
     module = spy.Module.load_from_file(device, os.path.join(curr_dir, "slang", "trace_geodesic.slang"))
 
-    # Create a buffer of particles (.as_struct ensures proper Python typing)
-    particles = module.Mesh()
+    mesh = load_mesh(module, device)
+
+
+if __name__ == "__main__":
+    main()
