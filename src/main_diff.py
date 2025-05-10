8004e9c5e928d26b2f4cb192083e360580004af2
Etyl <etyl.sani@gmail.com>
2025-05-10 17:32:37 +0100

    fix: main_diff

--- src/main_diff.py
+++ src/main_diff.py
@@ -18,45 +18,10 @@ def set_seed(seed: int):
     torch.manual_seed(seed)
     np.random.seed(seed)
 
-def tri_bary_coords(p0, p1, p2, p):
-    """Compute barycentric coordinates of p in the triangle (p0, p1, p2)."""
-    v0 = p1 - p0
-    v1 = p2 - p0
-    v2 = p - p0
-    
-    d00 = torch.dot(v0, v0)
-    d01 = torch.dot(v0, v1)
-    d11 = torch.dot(v1, v1)
-    d20 = torch.dot(v2, v0)
-    d21 = torch.dot(v2, v1)
-    
-    denom = d00 * d11 - d01 * d01
-    if abs(denom) < 1e-6:
-        return torch.tensor([1.0, 0.0, 0.0])
-    
-    v = (d11 * d20 - d01 * d21) / denom
-    w = (d00 * d21 - d01 * d20) / denom
-    u = 1.0 - v - w
-
-    return torch.stack([u, v, w]) # todo fix this
-
-def bary_to_uv(bary):
-    """Convert barycentric coordinates to UV coordinates."""
-    return bary[1:]
-
-def point_to_uv(mesh: Mesh, point: torch.Tensor, face:int) -> MeshPoint:
-    """Convert a point in 3D space to UV coordinates on the mesh."""
-    p0 = torch.tensor(mesh.positions[mesh.triangles[face][0]])
-    p1 = torch.tensor(mesh.positions[mesh.triangles[face][1]])
-    p2 = torch.tensor(mesh.positions[mesh.triangles[face][2]])
-    
-    bary = tri_bary_coords(p0, p1, p2, point)
-    return MeshPoint(face, bary_to_uv(bary))
 
 class DirNN(torch.nn.Module):
-    def __init__(self, mesh: Mesh, hidden_dim=128, cpus=1):
+    def __init__(self, mesh: Mesh, hidden_dim=128):
         super(DirNN, self).__init__()
-        self.cpus = cpus
         self.mesh = mesh
         self.mlp = torch.nn.Sequential(
             torch.nn.Linear(6,hidden_dim),
@@ -72,8 +37,7 @@ class DirNN(torch.nn.Module):
         points = torch.tensor(np.array([mesh_point.interpolate(self.mesh) for mesh_point in mesh_points]),dtype=torch.float64)
         x = torch.cat([points, normals], dim=1)
         dirs = self.mlp(x)
-        geodesics, new_points = trace_geodesics(self.mesh, mesh_points, dirs, cpus=self.cpus)
-        new_mesh_points = [point_to_uv(self.mesh, new_point, geodesic.end.face) for geodesic, new_point in zip(geodesics, new_points)]
+        new_mesh_points, new_points = trace_geodesics(self.mesh, mesh_points, dirs)
         return new_points, new_mesh_points
 
 
@@ -81,10 +45,10 @@ def score(x) -> torch.Tensor:
     return torch.mean(x[:,1])
 
 
-def main(n_points=100, epochs=50, iterations=3, cpus=None):
+def main(n_points=100, epochs=50, iterations=3):
     # mesh = create_tetrahedron()
     mesh = load_mesh_from_obj(os.path.join(DATA_DIR, "cat_head.obj"))
-    dir_nn = DirNN(mesh, cpus=cpus)
+    dir_nn = DirNN(mesh)
 
     optimizer = torch.optim.Adam(dir_nn.parameters(), lr=0.01)
     
