db7eda8a872d49c951001833a4df2113173d9ec7
Hippolyte Verninas <etyl.sani@gmail.com>
2025-05-21 11:26:09 +0100

    fix: instance of mesh

--- src/main_slang.py
+++ src/main_slang.py
@@ -6,7 +6,7 @@ from dataloader.mesh_loader import load_mesh_from_obj
 from constants import DATA_DIR
 
 
-def load_mesh(module:spy.Module, device:spy.Device):
+def load_meshes(module:spy.Module, device:spy.Device):
     mesh = load_mesh_from_obj(os.path.join(DATA_DIR,"cat_head.obj"))
     
     positions = device.create_buffer(
@@ -35,9 +35,18 @@ def load_mesh(module:spy.Module, device:spy.Device):
         data=mesh.v2t,
     )
 
-    # TODO: this doesn't work
-    slang_mesh = module.Mesh(positions, triangles, adjacencies, triangle_normals, v2t)
-    return slang_mesh
+    slang_meshes = spy.InstanceList(
+        struct=module.Mesh.as_struct(),
+        data={
+            "positions":positions, 
+            "triangles":triangles, 
+            "adjacencies":adjacencies,
+            "triangle_normals":triangle_normals,
+            "v2t":v2t,
+        }
+    )
+
+    return slang_meshes
 
 
 
@@ -50,7 +59,20 @@ def main():
     # Load the module
     module = spy.Module.load_from_file(device, os.path.join(curr_dir, "slang", "trace_geodesic.slang"))
 
-    mesh = load_mesh(module, device)
+    meshes = load_meshes(module, device)
+
+    meshpoints = spy.InstanceList(
+        struct=module.MeshPoint.as_struct(),
+        data={
+            "face":spy.int1(0), 
+            "uv":spy.float2(0.3,0.3),
+        }
+    )
+
+    dirs = spy.float3(1,1,1)
+
+    # TODO: figure out how to call this
+    result = module.straighest_geodesic(meshes, meshpoints, dirs)
 
 
 if __name__ == "__main__":
